48639e24012aab123a7e4ddb10180beff1929b33
Tetsuy2 <mmirzazcmihai@gmail.com>
2025-10-22 08:24:04 +0300

    Nuke repository contents before reimport

--- Broker/Services/Interfaces/IXmlValidator.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Broker.Services.Interfaces
-{
-    public interface IXmlValidator
-    {
-        // Throws on invalid XML/XSD; returns true on valid.
-        bool Validate(string xml, string? xsdPath = null);
-    }
-}
